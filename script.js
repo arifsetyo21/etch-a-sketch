d012ca0c7aaa240f78104d73487539536e71a295
arifsetyo21 <arifsetyo19@gmail.com>
2019-12-23 00:52:39 +0700

    add rainbow background, clean up view, add feature erase, fix change mode and color

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let dimension = prompt('Masukkan ukuran grid(ex .16) :', 16);
 let grid_container = document.querySelector('.grid-container');
 
 let gridHeight = 502/dimension;
-grid_container.setAttribute('style', `display: grid;grid-template-columns: repeat(${dimension}, 1fr);grid-template-rows: repeat(${dimension}, 1fr);border: #2196F3 solid 5px;`)
+grid_container.setAttribute('style', `display: grid;grid-template-columns: repeat(${dimension}, 1fr);grid-template-rows: repeat(${dimension}, 1fr);`)
 
 /* NOTE Creating canvas with dimention */
 for (let i = 0; i < Math.pow(dimension, 2); i++) {
@@ -15,14 +15,32 @@ for (let i = 0; i < Math.pow(dimension, 2); i++) {
 
 let color = '#000000';
 let type = 'single';
+let opacity_first = 0.1;
+
+document.querySelector('#eraseBtn').addEventListener('click', function (e) {
+   type = 'single'
+   color = 'ffffff'
+   opacity_first = 0;
+})
+
+let colorPicker = document.querySelector('#colorPicker');
+
+colorPicker.addEventListener('focusout', (e) => {
+   type = 'single';
+   color = colorPicker.value;
+   opacity_first = 0.1;
+})
 
 document.querySelector('#black').addEventListener('click', (e) => {
    type = 'single';
    color = 'black';
+   opacity_first = 0.1;
 })
 
 document.querySelector('#rainbow').addEventListener('click', (e) => {
    type = 'rainbow';
+   color = 'black';
+   opacity_first = 0.1;
 })
 
 /* NOTE Create canvas */
@@ -43,26 +61,35 @@ grids.forEach(function(grid) {
    
    /* NOTE Event when mouse click */
    grid.addEventListener('click', function (e) {
-      paintGrid(grid, color, type);
+      paintGrid(grid, color, type, opacity_first);
    });
 
    /* NOTE Event mouseover like hover */
    grid.addEventListener('mouseover', function(e) {
       /* NOTE if button using left click */
       if (e.buttons === 1) {
-         paintGrid(grid, color, type);
+         paintGrid(grid, color, type, opacity_first);
       }
    });
 })
 
 /* NOTE Create function for change grid color */
-function changeGrid(grid, color = '#000000'){
-   let style = getComputedStyle(grid);
-   let opacity = parseFloat(style.opacity);
-   // let display = style.
-   opacity += parseFloat(0.1);
-   grid.setAttribute('style', `background-color: ${color}; opacity: ` + opacity);
-   // element.style.cssText = "background-color: black";
+function changeGrid(grid, color = '#000000', opacity){
+   if (opacity === 0) {
+      let style = getComputedStyle(grid);
+      let opacity = parseFloat(style.opacity);
+      // let display = style.
+      opacity = parseFloat(0);
+      grid.setAttribute('style', `display: block; background-color: ${color}; height: ${gridHeight}px; opacity: ` + opacity);
+      // element.style.cssText = "background-color: black";
+   } else {
+      let style = getComputedStyle(grid);
+      let opacity = parseFloat(style.opacity);
+      // let display = style.
+      opacity += parseFloat(0.1);
+      grid.setAttribute('style', `display: block; background-color: ${color}; height: ${gridHeight}px; opacity: ` + opacity);
+      // element.style.cssText = "background-color: black";
+   }
 }
 
 function getRandomColor(){
@@ -74,13 +101,13 @@ function getRandomColor(){
    return color;
 }
 
-function paintGrid(grid, color, type){
+function paintGrid(grid, color, type, opacity){
 
    if (type == 'rainbow') {
       let warna = getRandomColor();
-      changeGrid(grid, warna);
+      changeGrid(grid, warna, opacity);
    } else {
-      changeGrid(grid, color);
+      changeGrid(grid, color, opacity);
    }
 
 }
